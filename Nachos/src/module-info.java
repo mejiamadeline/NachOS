cb916d6293a309d66102f1e99fe42d120a22e9c4
Maddie <mejiamadeline23@gmail.com>
2021-10-07 00:26:48 -0700

    test

---
+++ Nachos/src/module-info.java
@@ -0,0 +1,11 @@
+module nacho {
+	exports nachos.userprog;
+	exports nachos.vm;
+	exports nachos.ag;
+	exports nachos.network;
+	exports nachos.threads;
+	exports nachos.machine;
+	exports nachos.security;
+
+	requires java.desktop;
+}
